d29ae73187eed605a53dff63a50de7b514a1f347
Gonza <Gonza@DESKTOP-O4RO760>
2023-11-06 22:04:29 -0300

    Creacion ventanas - GA

---
+++ src/dominio/Persona.java
@@ -0,0 +1,25 @@
+
+import interfaz.*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Gonza
+package dominio;
+
+import interfaz.*;
+
+/**
+ *
+ * @author Gonza
+ */
+public class Persona {
+    public static void main(String[]args) {
+        HistorialPostulante panel = new HistorialPostulante();
+        panel.setVisible(true);
+    }
+ }
+
